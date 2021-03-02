87f9e0ab87723add96b0cf394fe905ecb183839b
kan0ne <kan0neego@gmail.com>
2021-03-02 16:31:04 +0700

    Add main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function upperVar() {
+    // Объявление (let = arr;) переменной arr
+    for(let i = 0; i < arr.length; i++) {
+        console.log('Number:' + ' ' + arr[i]);
+    };
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Инициализация переменной arr
+};
+
+upperVar();
