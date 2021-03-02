56c8ff50c5be35eed687d14ffa0a5fa677b69493
kan0ne <kan0neego@gmail.com>
2021-03-02 20:08:43 +0700

    Add variable checking on undefined

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 'use strict';
 
 function upperVar() {
-    // Объявление (let = arr;) переменной arr
-    for(let i = 0; i < arr.length; i++) {
-        console.log('Number:' + ' ' + arr[i]);
+  // Объявление (let = arr;) переменной arr
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Инициализация переменной arr
+  // eslint-disable-next-line no-use-before-define
+  if (top === undefined) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(`Number: ${arr[i]}`);
     };
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Инициализация переменной arr
+  }
+  let top;
 };
 
 upperVar();
